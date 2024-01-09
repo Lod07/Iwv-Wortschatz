572a21c7c7952b5b4654d006bd8883163b35ceba
baumdude <Caligorax@gmail.com>
2024-01-09 17:54:54 +0100

    Ordnen Synonyme Paare

--- IWV_studentProject/utils.py
+++ IWV_studentProject/utils.py
@@ -6,42 +6,8 @@ dateipfad = 'D:/documents/Uni/MI_M/IWV/git/Iwv-Wortschatz/IWV_studentProject/xml
 unique = []
 unique_final = {}
 
-'''def getSynonymsWikitionary(token):
-    r = requests.get(f"https://de.wiktionary.org/wiki/{token}")
-    if("Dieser Eintrag existiert noch nicht!" in r.text):
-        return []
-    else:
-        if not "Synonyme" in r.text:
-            return []
-        
 
-        blob = r.text.split("Synonyme:")[1]
-        beginning = blob.split("<dd>")[1]
-        end_index = beginning.index("</dd>")
-        result = beginning[:end_index]
-        
-        # < a ... > ... < /a >  BOESE: </i>
-
-        lines = result.split("</a>")
-        output = []
-        for line in lines:
-            # print(line)
-            if "<i>" in line:
-                if not "</i>" in line:
-                    continue
-
-                if not line.rfind('</i>') < line.rfind('<a'):
-                    continue
-            
-            index = line.rfind('>') + 1
-
-            if(index < len(line) - 1):
-                output.append(line[index:])
-
-        return output'''
-
-
-def getSynonymsWikitionary(token):
+def getSynonymsWiktionary(token):
     r = requests.get(f"https://de.wiktionary.org/wiki/{token}")
     if "Dieser Eintrag existiert noch nicht!" in r.text:
         return []
@@ -75,29 +41,31 @@ def getSynonymsWikitionary(token):
 
     return output
 
-
-
 if __name__ == "__main__":
-
     with open(dateipfad, 'r', encoding='utf-8') as datei:
         unique = datei.read().splitlines()
 
     print(unique)
 
-    unique_final = []
+    synonyms_pairs = []
 
     for wort in unique:
-        synonyme = getSynonymsWikitionary(wort)
+        synonyme = getSynonymsWiktionary(wort)
         
         # Hier werden die gefundenen Synonyme zur Liste hinzugefügt
-        unique_final.extend(synonyme)
+        synonyms_pairs.append(f"{wort}: {', '.join(synonyme)}")
 
     # Ergebnisse ausgeben
-    print("Synonyme:")
-    for element in unique_final:
-        print(element)
+    print("Synonyms-pairs:")
+    for pair in synonyms_pairs:
+        print(pair)
 
-    output_dateipfad = 'D:/documents/Uni/MI_M/IWV/git/Iwv-Wortschatz/IWV_studentProject/xmlfiles/Synonyme_Liste.txt'
-    with open(output_dateipfad, 'w', encoding='utf-8') as output_datei:
-        for element in unique_final:
-            output_datei.write(element + '\n')
+    output_dateipfad_synonyms = 'D:/documents/Uni/MI_M/IWV/git/Iwv-Wortschatz/IWV_studentProject/xmlfiles/Synonyme_Liste.txt'
+    output_dateipfad_pairs = 'D:/documents/Uni/MI_M/IWV/git/Iwv-Wortschatz/IWV_studentProject/xmlfiles/Synonyms_Pairs_Liste.txt'
+    
+    with open(output_dateipfad_synonyms, 'w', encoding='utf-8') as output_datei_synonyms, open(output_dateipfad_pairs, 'w', encoding='utf-8') as output_datei_pairs:
+        for synonym in unique_final:
+            output_datei_synonyms.write(synonym + '\n')
+        
+        for pair in synonyms_pairs:
+            output_datei_pairs.write(pair + '\n')
