77be8a93f8ee500542a9009377007f87a8594e32
baumdude <Caligorax@gmail.com>
2024-02-15 18:54:44 +0100

    stand jetzt
    15.02.

--- IWV_studentProject/studentcode_Timur2.py
+++ IWV_studentProject/studentcode_Timur2.py
@@ -13,18 +13,6 @@ import pandas as pd
 
 import utils
 
-
-import psutil
-import threading
-import time
-import datetime
-import timeit
-from queue import Queue
-
-import cProfile
-import pstats
-from pstats import SortKey
-
 from performance_tracker import CProfiler, PerformanceTracker
 
 # Pfade in besser
@@ -32,114 +20,86 @@ from pathlib import Path
 # LADEBALKEN
 from tqdm import tqdm
 
-ENDINGS = ['heit', 'ie', 'ik', 'ion', 'ismus', 'ität', 'keit', 'nz', 'tur', 'ung']
- 
-#@profile
-def count_words_and_pos_tags(all_text, nlp):
-    # spaCy für POS-Tagging verwenden
-    doc = nlp(all_text)
-
-
-    # Dictionary zur Verfolgung der Anzahl der Wörter pro Wortart
-    pos_count = {}
-
-    # Liste für alle individuellen Wörter mit ihrer Häufigkeit und Wortart
-    word_list = []
-
-    for token in doc:
-        # Ignoriere Satzzeichen und Leerzeichen
-        if not token.is_punct and not token.is_space:
-            # Wort und Wortart abrufen
-            word = token.text.lower()  # kleinschreiben, um Groß-/Kleinschreibung zu ignorieren
-            pos = token.pos_
-            tag = token.tag_
-
-            
-            # Anzahl der Wörter pro Wortart zählen
-            pos_count[pos] = pos_count.get(pos, 0) + 1
-
-            # enferne sachen wie 1. 2. 3. usw.
-            if word[-1] == "." and tag == "ADJ":
-                continue
-            
-            if pos == "NUM":
-                continue
-
-            # Informationen zu jedem Wort speichern
-            word_list.append((capitalize_word(word, pos), pos, tag))
-
-    return pos_count, word_list
-
-
-def find_unique_words(word_list):
-    # Verwende Counter, um die Häufigkeit jedes Wortes zu zählen
-    word_counter = Counter(word for word, _, _ in word_list)
-
-    # Dictionary zum Verfolgen der Präfixe und ihrer zugehörigen Wörter
-    prefix_to_words = {}
-    
-
-    for word, count in word_counter.items():
-        if count == 1 and not word.endswith(".") and not word.startswith(".") and not any(char.isdigit() for char in word):
-            # Präfix extrahieren
-            word_prefix = get_prefix(word)
-
-            # Überprüfen, ob das aktuelle Präfix bereits existiert
-            if word_prefix in prefix_to_words:
-                # Das aktuelle Wort wird zum Dictionary hinzugefügt, um alle Wörter mit dem gleichen Präfix zu speichern
-                prefix_to_words[word_prefix].append(word)
-            else:
-                # Das aktuelle Wort wird zum Dictionary hinzugefügt
-                prefix_to_words[word_prefix] = [word]
-
-    # Liste der einzigartigen Wörter erstellen (nur Wörter mit einmaligem Präfix)
-    unique_words = [words[0] for words in prefix_to_words.values() if len(words) == 1]
-
-    return sorted(unique_words)
-
-def get_prefix(word):
-    # Hier können Sie die Logik für das Extrahieren des Präfixes anpassen
-    # In diesem Beispiel wird das Präfix als die ersten drei Buchstaben des Wortes genommen
-    return word[:3].lower()
-
-
-
-
-
-
-def find_noun_endings(noun_list):
-    # Listen für Substantive mit den gesuchten Endungen
-    ending_lists = {ending: [] for ending in ENDINGS}
-
-    for noun in noun_list:
-        for ending in ENDINGS:
-            if noun.endswith(ending):
-                ending_lists[ending].append(noun)
-
-    return ending_lists
-
-# Funktion zur Überprüfung der Großschreibung
-def capitalize_word(word, pos):
-    if pos in ['NOUN', 'PROPN', 'NE', 'NNE']:
-        return word.capitalize()
-    return word
-
-
 def main():
 
     tr=PerformanceTracker()
-        
+
+    ENDINGS = ['heit', 'ie', 'ik', 'ion', 'ismus', 'ität', 'keit', 'nz', 'tur', 'ung']
+    
+
+    def count_words_and_pos_tags(all_text, nlp):
+        # spaCy für POS-Tagging verwenden
+        doc = nlp(all_text)
+
+
+        # Dictionary zur Verfolgung der Anzahl der Wörter pro Wortart
+        pos_count = {}
+
+        # Liste für alle individuellen Wörter mit ihrer Häufigkeit und Wortart
+        word_list = []
+
+        for token in doc:
+            # Ignoriere Satzzeichen und Leerzeichen
+            if not token.is_punct and not token.is_space:
+                # Wort und Wortart abrufen
+                word = token.text.lower()  # kleinschreiben, um Groß-/Kleinschreibung zu ignorieren
+                pos = token.pos_
+                tag = token.tag_
+
+                
+                # Anzahl der Wörter pro Wortart zählen
+                pos_count[pos] = pos_count.get(pos, 0) + 1
+
+                # enferne sachen wie 1. 2. 3. usw.
+                if word[-1] == "." and tag == "ADJ":
+                    # TODO: hier dringend mal nachschauen dass keine falschen wörter entfernt werden!!!!
+                    continue
+                
+                if pos == "NUM":
+                    continue
+
+                # Informationen zu jedem Wort speichern
+                word_list.append((capitalize_word(word, pos), pos, tag))
+
+        return pos_count, word_list
+
+    def find_unique_words(word_list):
+        # Verwende Counter, um die Häufigkeit jedes Wortes zu zählen
+        word_counter = Counter(word for word, _, _ in word_list)
+
+        # Liste für Wörter, die nur einmal vorkommen
+        unique_words = sorted([word for word, count in word_counter.items() if count == 1])
+        return unique_words
+
+
+
+    def find_noun_endings(noun_list):
+        # Listen für Substantive mit den gesuchten Endungen
+        ending_lists = {ending: [] for ending in ENDINGS}
+
+        for noun in noun_list:
+            for ending in ENDINGS:
+                if noun.endswith(ending):
+                    ending_lists[ending].append(noun)
+
+        return ending_lists
+
+    # Funktion zur Überprüfung der Großschreibung
+    def capitalize_word(word, pos):
+        if pos in ['NOUN', 'PROPN', 'NE', 'NNE']:
+            return word.capitalize()
+        return word
+
 
     # Gesamtanzahl aller Wörter
     total_word_count = 0
 
-    # spaCy-Modell laden (deutsch)
     tr.startTimeSection('ModelLoading')
-    tr.startSystemMonitor()
+    tr.startSystemMonitor(250)
+    # spaCy-Modell laden (deutsch)
     nlp = spacy.load("de_core_news_lg")
     tr.stopSystemMonitor()
     tr.endTimeSection('ModelLoading')
-
 
     # Dictionary zur Verfolgung der Gesamtanzahl der Wörter pro Wortart
     total_pos_count = {}
@@ -155,9 +115,6 @@ def main():
 
     dataset = LSNewsData(datasetPath, options)
     ending_list_per_document = []
-
-    tr.startTimeSection('WordsEinlesen')
-    tr.startSystemMonitor()
 
     for i in tqdm(range(len(dataset))):
         # Anzahl der Wörter und POS-Tags ermitteln
@@ -178,16 +135,10 @@ def main():
         document_ending_lists = find_noun_endings(document_nouns)
         ending_list_per_document.append(document_ending_lists)
 
-    tr.stopSystemMonitor()
-    tr.endTimeSection('WordsEinlesen')
-
     ending_means = []
     ending_max = []
     ending_std_devs = []
 
-    
-    tr.startTimeSection('EndungenBerechnen')
-    tr.startSystemMonitor()
     for ending in ENDINGS:
         counts = []
         for doc_elists in ending_list_per_document:
@@ -205,36 +156,44 @@ def main():
         ending_means.append(counts_array.mean())
         ending_max.append(counts_array.max())
         ending_std_devs.append(counts_array.std())
-    tr.stopSystemMonitor()
-    tr.endTimeSection('EndungenBerechnen')
+        
 
 
 
-    # # Erstellen des bar plots mit Error-Bars
-    # plt.figure(figsize=(10, 6))
-    # sns.barplot(x=ENDINGS, y=ending_means)
+
+    # Liste für alle Substantive
+    all_nouns = [word for word, pos, _ in total_word_list if pos in ['NOUN', 'PROPN', 'NE', 'NNE']]
+    ending_lists = find_noun_endings(all_nouns)
+    ending_counts = {ending: len(nouns) for ending, nouns in ending_lists.items()}
 
 
-    # # Iteriere über jeden Balken und füge einen individuellen Fehlerbalken hinzu
-    # for i, mean in enumerate(ending_means):
-    #     std_dev = ending_std_devs[i]
-    #     plt.errorbar(i, mean, yerr=std_dev, color='black', capsize=5)  
+    # Your data
+    suffixes = ['heit', 'ie', 'ik', 'ion', 'ismus', 'ität', 'keit', 'nz', 'tur', 'ung']
+    data = {
+        'heit': [0.10952380952380952, 0.4704655869250434, 0, 5, 5, 0.2213378684807256],
+        'ie': [0.18412698412698414, 1.0754410463584194, 0, 14, 14, 1.156573444192492],
+        'ik': [0.2222222222222222, 0.9569203984814946, 0, 10, 10, 0.9156966490299824],
+        'ion': [0.36666666666666664, 1.465096562178731, 0, 18, 18, 2.1465079365079367],
+        'ismus': [0.006349206349206349, 0.11250822315345618, 0, 2, 2, 0.012658100277147895],
+        'ität': [0.007936507936507936, 0.13189893593995067, 0, 3, 3, 0.017397329302091213],
+        'keit': [0.01904761904761905, 0.23152849106939466, 0, 4, 4, 0.053605442176870764],
+        'nz': [0.07301587301587302, 0.7545010648706233, 0, 14, 14, 0.5692718568909045],
+        'tur': [0.07936507936507936, 0.6349999950403024, 0, 12, 12, 0.4032249937011841],
+        'ung': [1.5492063492063493, 2.3080172907850716, 0, 17, 17, 5.3269438145628625],
+    }
 
-    # plt.title("Endungen Substantive Mittelwerte mit Standardabweichung")
-    # plt.xlabel("Endungen")
-    # plt.ylabel("Anzahl")
+    # # Create a DataFrame
+    # df = pd.DataFrame(data)
+
+    # # Plotting
+    # plt.figure(figsize=(14, 8))
+    # sns.boxplot(data=df, width=0.7, palette='Set2')
+    # plt.title('Boxplot of Additional Statistics for each Ending')
     # plt.show()
 
-    '''
-    for ending in ENDINGS:    
-        sns.boxplot(data = counts)
-        plt.title(f'Boxplot für Endung "{ending}"')
-        plt.show()
-    '''
 
     # # Daten für Boxplot vorbereiten
     # boxplot_data = {ending: pd.Series([len(doc_elists[ending]) for doc_elists in ending_list_per_document]) for ending in ENDINGS}
-
 
 
     # # Daten für Boxplot in DataFrame umwandeln
@@ -262,43 +221,15 @@ def main():
     # plt.ylabel("Anzahl")
     # plt.show()
 
-    '''
-    boxplot_data = {ending: [] for ending in ENDINGS}
 
-    for doc_elists in ending_list_per_document:
-        for ending in ENDINGS:
-            noun_count = len(doc_elists[ending])
-            # Überprüfen, ob die Liste leer ist
-            #if noun_count > 0:
-            #    boxplot_data[ending].append(noun_count)
-            #else:
-            #    boxplot_data[ending].append(np.nan)
-
-    # Daten für Boxplot in DataFrame umwandeln
-    data = pd.DataFrame(boxplot_data)
-
-    # Boxplot erstellen
-    plt.figure(figsize=(12, 8))
-    sns.boxplot(data=data, flierprops = dict(markerfacecolor = '0.50', markersize = 2))
-    # Hier setzt du die Einteilung der y-Achse in Zweierschritten
-    plt.yticks(np.arange(0, max(counts_array)+1, 2))
-    plt.ylim(0,16)
-    plt.title("Boxplot für Substantiv-Endungen")
-    plt.xlabel("Endungen")
-    plt.ylabel("Anzahl")
-    plt.show()
-
-    '''
-
-
-
-    # sns.barplot(x=ENDINGS, y=ending_means)
+    # sns.barplot(x=ENDINGS, y=ending_means, errorbar='sd')
     # plt.title("Endungen Substantive Mittelwerte")
     # plt.xlabel("Endungen")
     # plt.ylabel("Anzahl")
     # plt.show()
 
-    # sns.barplot(x=ENDINGS, y=ending_max)
+
+    # sns.barplot(x=ENDINGS, y=ending_max, errorbar='sd')
     # plt.title("Endungen Substantive Maxima")
     # plt.xlabel("Endungen")
     # plt.ylabel("Anzahl")
@@ -312,27 +243,8 @@ def main():
 
     print(len(total_word_list))
 
-
-    tr.startTimeSection('finduniquewords')
-    tr.startSystemMonitor()
     # Identifiziere Wörter, die nur einmal vorkommen
     unique_words = find_unique_words(total_word_list)
-    #unique_words = [word for word in unique_words if not word.endswith(".")]
-    tr.stopSystemMonitor()
-    tr.endTimeSection('finduniquewords')
-
-    # Liste für alle Substantive
-    all_nouns = [word for word, pos, _ in total_word_list if pos in ['NOUN', 'PROPN', 'NE', 'NNE']]
-
-    tr.startTimeSection('IdentifiziereSubstantive')
-    tr.startSystemMonitor()
-    # Identifiziere Substantive mit den gesuchten Endungen
-    ending_lists = find_noun_endings(all_nouns)
-    tr.stopSystemMonitor()
-    tr.endTimeSection('IdentifiziereSubstantive')
-
-
-
 
     # Ausgabe 
     output_path = os.path.join(str(datasetPath), "Liste_Aufgabe_1.txt")
@@ -341,7 +253,6 @@ def main():
         output_file.write("Anzahl Wortarten:\n")
         for pos, count in total_pos_count.items():
             output_file.write(f"{pos}: {count}\n")
-
 
 
         # daten umformatieren hier
@@ -360,6 +271,8 @@ def main():
         for word_info in final:
             output_file.write(str(word_info) + " " + str(final[word_info]) + "\n")
 
+        
+
         output_file.write("\nSubstantive mit den gesuchten Endungen:\n")
         for ending, nouns in ending_lists.items():
             output_file.write(f"{ending}: {len(nouns)} - {nouns}\n")
@@ -367,8 +280,6 @@ def main():
     print(f"Ausgabe wurde in der Datei {output_path} gespeichert.")
 
 
-
-    #Herausfiltern unique words
     output_path = os.path.join(str(datasetPath), "Liste_Aufgabe_1_unique_words.txt")
     with open(output_path, 'w', encoding='utf-8') as output_file2:
         for unique_word in unique_words:
@@ -378,11 +289,8 @@ def main():
 
 
 
-    # #Visualisierung 
-
     # # Visualisierung Endungen Substantive
-    # ending_counts = {ending: len(nouns) for ending, nouns in ending_lists.items()}
-    # sns.barplot(x=list(ending_counts.keys()), y=list(ending_counts.values()))
+    # sns.barplot(x=list(ending_counts.keys()), y=list(ending_counts.values()), errorbar='sd')
     # plt.title("Anzahl der Substantive mit den gesuchten Endungen")
     # plt.xlabel("Endungen")
     # plt.ylabel("Anzahl")
@@ -395,25 +303,11 @@ def main():
     # ending_counts_relative = {ending: len(words) for ending, words in ending_lists_relative.items()}
 
     # # Plot
-    # sns.barplot(x=list(ending_counts_relative.keys()), y=list(ending_counts_relative.values()))
+    # sns.barplot(x=list(ending_counts_relative.keys()), y=list(ending_counts_relative.values()), errorbar='sd')
     # plt.title("Anzahl der Substantive mit den gesuchten Endungen (ohne Duplikate)")
     # plt.xlabel("Endungen")
     # plt.ylabel("Anzahl")
     # plt.show()
-
-
-
-    '''
-    # Wordcloud erstellen
-    wordcloud_text = ' '.join([word for word, _, _ in total_word_list])
-    wordcloud = WordCloud(width=800, height=400, random_state=21, max_font_size=110, background_color='white').generate(wordcloud_text)
-
-    # Wordcloud anzeigen
-    plt.figure(figsize=(10, 5))
-    plt.imshow(wordcloud, interpolation="bilinear")
-    plt.axis('off')
-    plt.show()
-    '''
     tr.close()
 
 if __name__ == "__main__":
@@ -422,7 +316,4 @@ if __name__ == "__main__":
 
     cp = CProfiler()
     cp.profileCall(main)
-
-
-
 
